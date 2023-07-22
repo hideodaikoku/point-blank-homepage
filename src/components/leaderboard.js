a95f5f988440b376278455fedb80286b62b3efc2
Hideo <neildaikoku@gmail.com>
2023-07-22 19:26:32 +0900

    add leaderboard

--- src/components/leaderboard.js
+++ src/components/leaderboard.js
@@ -1,21 +1,94 @@
 import * as React from "react"
-import {
-  Container,
-  Section,
-  Box,
-  Kicker,
-  Heading,
-} from "./ui"
+import { Container, Section, Box, Kicker, Heading, Flex, Text } from "./ui"
+import { TableContainer, Table, Th, Td, Image } from "./leaderboard.css"
 
 const leaderboard = [
   {
-    profilePicture: "",
-    firstName: "",
-    country: "",
-    accountSize: "",
-    monthlyGainPercent: "",
-    monthlyGainDollars: "",
-  }
+    profilePicture: "https://i.pravatar.cc/30",
+    firstName: "Kumar",
+    country: "India",
+    accountSize: "$500,000",
+    monthlyGainPercent: 20,
+    monthlyGainDollars: "$1,000",
+  },
+  {
+    profilePicture: "https://i.pravatar.cc/30",
+    firstName: "Kumar",
+    country: "India",
+    accountSize: "$500,000",
+    monthlyGainPercent: 20,
+    monthlyGainDollars: "$1,000",
+  },
+  {
+    profilePicture: "https://i.pravatar.cc/30",
+    firstName: "Kumar",
+    country: "India",
+    accountSize: "$500,000",
+    monthlyGainPercent: 20,
+    monthlyGainDollars: "$1,000",
+  },
+  {
+    profilePicture: "https://i.pravatar.cc/30",
+    firstName: "Kumar",
+    country: "India",
+    accountSize: "$500,000",
+    monthlyGainPercent: 20,
+    monthlyGainDollars: "$1,000",
+  },
+
+  {
+    profilePicture: "https://i.pravatar.cc/30",
+    firstName: "Kumar",
+    country: "India",
+    accountSize: "$500,000",
+    monthlyGainPercent: 20,
+    monthlyGainDollars: "$1,000",
+  },
+
+  {
+    profilePicture: "https://i.pravatar.cc/30",
+    firstName: "Kumar",
+    country: "India",
+    accountSize: "$500,000",
+    monthlyGainPercent: 20,
+    monthlyGainDollars: "$1,000",
+  },
+
+  {
+    profilePicture: "https://i.pravatar.cc/30",
+    firstName: "Kumar",
+    country: "India",
+    accountSize: "$500,000",
+    monthlyGainPercent: 20,
+    monthlyGainDollars: "$1,000",
+  },
+
+  {
+    profilePicture: "https://i.pravatar.cc/30",
+    firstName: "Kumar",
+    country: "India",
+    accountSize: "$500,000",
+    monthlyGainPercent: 20,
+    monthlyGainDollars: "$1,000",
+  },
+
+  {
+    profilePicture: "https://i.pravatar.cc/30",
+    firstName: "Kumar",
+    country: "India",
+    accountSize: "$500,000",
+    monthlyGainPercent: 20,
+    monthlyGainDollars: "$1,000",
+  },
+
+  {
+    profilePicture: "https://i.pravatar.cc/30",
+    firstName: "Kumar",
+    country: "India",
+    accountSize: "$500,000",
+    monthlyGainPercent: 20,
+    monthlyGainDollars: "$1,000",
+  },
 ]
 
 export default function Education() {
@@ -29,6 +102,44 @@ export default function Education() {
               LEADERBOARD
             </Heading>
           </Box>
+          <Flex variant="center">
+            <div className={TableContainer}>
+              <table className={Table}>
+                <thead>
+                  <tr>
+                    <th className={Th}></th>
+                    <th className={Th}>Rank</th>
+                    <th className={Th}>First Name</th>
+                    <th className={Th}>Country</th>
+                    <th className={Th}>Account Size</th>
+                    <th className={Th}>Monthly Gain (%)</th>
+                    <th className={Th}>Monthly Gain ($)</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {leaderboard.map((row, i) => (
+                    <tr key={i}>
+                      <td>
+                        <Flex variant="center">
+                          <img
+                            src={row.profilePicture}
+                            alt={`Profile of ${row.firstName}`}
+                            className={Image}
+                          />
+                        </Flex>
+                      </td>
+                      <td className={Td}>{i + 1}</td>
+                      <td className={Td}>{row.firstName}</td>
+                      <td className={Td}>{row.country}</td>
+                      <td className={Td}>{row.accountSize}</td>
+                      <td className={Td}>{row.monthlyGainPercent}%</td>
+                      <td className={Td}>{row.monthlyGainDollars}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </Flex>
         </Box>
       </Container>
     </Section>
